82f8fe5a53c325b4cf60afc2d7b229f57f682e5c
iamnikhilranjan <nikhil21907@gmail.com>
2025-05-20 23:27:24 +0530

    weather-agent

--- weather-agent.py
+++ weather-agent.py
@@ -1,93 +1,120 @@
-from dotenv import load_dotenv
-from openai import OpenAI
-
 import json
+import requests
+from dotenv import load_dotenv
+from langfuse.decorators import observe
+from langfuse.openai import openai
+import os
+
+
+from langsmith import traceable
 load_dotenv()
 
-client = OpenAI()
+# client = wrap_openai(OpenAI())
+client = openai.Client()
 
+@observe()
+def run_command(command):
+    result = os.system(command=command)
+    return result
+
+
+@observe()
 def get_weather(city: str):
-    #TODO! : do an actual API call
-    return "32 degree celcius"
+    # TODO!: Do an actual API Call
+    print("🔨 Tool Called: get_weather", city)
+    
+    url = f"https://wttr.in/{city}?format=%C+%t"
+    response = requests.get(url)
 
-available_tools = {
-    "get_weather" : {
-        "fn" : get_weather,
-        "description" : "Takes the city name as input and return the current weather of the city"
+    if response.status_code == 200:
+        return f"The weather in {city} is {response.text}."
+    return "Something went wrong"
+
+@observe()
+def add(x, y):
+    print("🔨 Tool Called: add", x, y)
+    return x + y
+
+avaiable_tools = {
+    "get_weather": {
+        "fn": get_weather,
+        "description": "Takes a city name as an input and returns the current weather for the city"
+    },
+    "run_command": {
+        "fn": run_command,
+        "description": "Takes a command as input to execute on system and returns ouput"
     }
 }
 
-system_prompt = '''
-   You are an helpful AI assistant who is specialized in resolving user querry. 
-   You work on start , plan, action, observe mode.
-   for the given user query and availanbel tools, plan the step by step execution, based on the planning, select the relevant tool from the available tool. and based on the tool selection you perform an action to call the tool. Wait for the observation and based on the obdervation from the tool call resolve the user querry. 
+system_prompt = f"""
+    You are an helpfull AI Assistant who is specialized in resolving user query.
+    You work on start, plan, action, observe mode.
+    For the given user query and available tools, plan the step by step execution, based on the planning,
+    select the relevant tool from the available tool. and based on the tool selection you perform an action to call the tool.
+    Wait for the observation and based on the observation from the tool call resolve the user query.
 
-   Rules: 
-   - Follow the output JSON format.
-   - Always perform one step at a time and wait for the next input.
-   - Carefully analyse the user query.
+    Rules:
+    - Follow the Output JSON Format.
+    - Always perform one step at a time and wait for next input
+    - Carefully analyse the user query
 
-   Output JSON Format: 
-   {{
-     "step": "string",
-     "content": "string",
-     "function": "The name of function if the step is action",
-     "input": "The input parameter fot the function"
-   }}
+    Output JSON Format:
+    {{
+        "step": "string",
+        "content": "string",
+        "function": "The name of function if the step is action",
+        "input": "The input parameter for the function",
+    }}
 
-   Available Tools: 
-
-   Example: 
-   User Query: What is the user of new York ?
-   Output: {{"step": "plan", "content": "The user is interested in weather data of new york"}}
-   Output: {{"step": "plan", "content": "From the available tools I should call get_weather"}}
-   Output: {{"step": "action", "function": "get_weather", "input": "new york"}}
-   Output: {{"step": "observe", "output": "12 Degree cel"}}
-   Output: {{"step": "output", "content": "The weather for new york seems to be 12 degrees."}}
-
-
-'''
+    Available Tools:
+    - get_weather: Takes a city name as an input and returns the current weather for the city
+    - run_command: Takes a command as input to execute on system and returns ouput
+    
+    Example:
+    User Query: What is the weather of new york?
+    Output: {{ "step": "plan", "content": "The user is interseted in weather data of new york" }}
+    Output: {{ "step": "plan", "content": "From the available tools I should call get_weather" }}
+    Output: {{ "step": "action", "function": "get_weather", "input": "new york" }}
+    Output: {{ "step": "observe", "output": "12 Degree Cel" }}
+    Output: {{ "step": "output", "content": "The weather for new york seems to be 12 degrees." }}
+"""
 
 messages = [
-    {"role": "system", "content": system_prompt}
+    { "role": "system", "content": system_prompt }
 ]
 
-user_query = input('> ')
-messages.append({"role": "user", "content": user_query})
+while True:
+    user_query = input('> ')
+    messages.append({ "role": "user", "content": user_query })
 
-while True: 
-    response = client.chat.completions.create(
-        model = "gpt-4o",
-        response_format={"type": "json_object"},
-        messages = messages
-    )
+    while True:
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            response_format={"type": "json_object"},
+            messages=messages
+        )
 
-    parsed_ouput = json.loads(response.choices[0].message.content)
-    messages.append({"role": "assistant", "content": json.dumps(parsed_ouput)})
+        parsed_output = json.loads(response.choices[0].message.content)
+        messages.append({ "role": "assistant", "content": json.dumps(parsed_output) })
 
-    if parsed_ouput.get("step") == "plan": 
-        print(f"brain: {parsed_ouput.get("content")}")
-        continue
+        if parsed_output.get("step") == "plan":
+            print(f"🧠: {parsed_output.get("content")}")
+            continue
+        
+        if parsed_output.get("step") == "action":
+            tool_name = parsed_output.get("function")
+            tool_input = parsed_output.get("input")
 
-    if parsed_ouput.get("step") == "action":
-        tool_name = parsed_ouput.get("function")
-        tool_input = parsed_ouput.get("input")
+            if avaiable_tools.get(tool_name, False) != False:
+                output = avaiable_tools[tool_name].get("fn")(tool_input)
+                messages.append({ "role": "assistant", "content": json.dumps({ "step": "observe", "output":  output}) })
+                continue
+        
+        if parsed_output.get("step") == "output":
+            print(f"🤖: {parsed_output.get("content")}")
+            break
 
-        if available_tools.get(tool_name, False) != False:
-            available_tools[tool_name].get("fn")(tool_input)
+
+    
 
 
-response = client.chat.completions.create(
-    model = "gpt-4o",
-    response_format={"type": "json_object"},
-    messages = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": "what is the current weather of Patyala?"},
-        {"role": "assistant", "content": json.dumps({"step": "plan", "content": "The user is asking for current weather in Patiala"})},
-        {"role": "assistant", "content": json.dumps({"step": "plan", "content": "From the available tools, I should call get_weather to obtain the weather information for patiala."})},
-        {"role": "assistant", "content": json.dumps({"step": "action", "Function": "grt_weather", "input": "Patiala"})},
-        {"role": "assistant", "content": json.dumps({"step": "observe", "output": "32 degree celcius!"})},
-    ]
-)
-
-print(response.choices[0].message.content)
